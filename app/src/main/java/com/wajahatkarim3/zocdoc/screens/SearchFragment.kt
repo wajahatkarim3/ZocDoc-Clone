6a3bf2f500324b2ca9d004b5c6a7e49405d70c5c
Wajahat Karim <wajahatkarim3@gmail.com>
2020-12-28 20:00:20 +0500

    Search screen animation done

--- app/src/main/java/com/wajahatkarim3/zocdoc/screens/SearchFragment.kt
+++ app/src/main/java/com/wajahatkarim3/zocdoc/screens/SearchFragment.kt
@@ -1,13 +1,18 @@
 package com.wajahatkarim3.zocdoc.screens
 
+import android.animation.ObjectAnimator
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.Animation
 import com.wajahatkarim3.zocdoc.R
+import com.wajahatkarim3.zocdoc.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
+
+    lateinit var bi: FragmentSearchBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +20,22 @@ class SearchFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false)
+        bi = FragmentSearchBinding.inflate(inflater, container, false)
+        return bi.root
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        setupViews()
+    }
+
+    fun setupViews() {
+        // Looping Green Shape
+        val imageViewAnimator = ObjectAnimator.ofFloat(bi.imgGreenShape,View.ROTATION, 360f)
+        imageViewAnimator.repeatCount = Animation.INFINITE
+        imageViewAnimator.repeatMode = ObjectAnimator.RESTART
+        imageViewAnimator.duration = 30_000
+        imageViewAnimator.start()
     }
 }
