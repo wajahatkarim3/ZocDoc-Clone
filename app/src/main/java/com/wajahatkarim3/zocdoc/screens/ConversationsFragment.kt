654af6b68f667042d0f583c2c9efbc903a6698b4
Wajahat Karim <wajahatkarim3@gmail.com>
2020-12-31 00:21:10 +0500

    Added doctor to patient chat

--- app/src/main/java/com/wajahatkarim3/zocdoc/screens/ConversationsFragment.kt
+++ app/src/main/java/com/wajahatkarim3/zocdoc/screens/ConversationsFragment.kt
@@ -1,12 +1,22 @@
 package com.wajahatkarim3.zocdoc.screens
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.cometchat.pro.constants.CometChatConstants
+import com.cometchat.pro.core.CometChat
+import com.cometchat.pro.core.ConversationsRequest
+import com.cometchat.pro.exceptions.CometChatException
+import com.cometchat.pro.models.Conversation
+import com.cometchat.pro.models.User
+import com.google.android.material.snackbar.Snackbar
 import com.wajahatkarim3.zocdoc.R
 import com.wajahatkarim3.zocdoc.databinding.FragmentConversationsBinding
+import com.wajahatkarim3.zocdoc.model.DoctorModel
+import listeners.OnItemClickListener
 
 class ConversationsFragment : Fragment() {
 
@@ -26,9 +36,42 @@ class ConversationsFragment : Fragment() {
         super.onActivityCreated(savedInstanceState)
 
         setupViews()
+        setLoggedInUI()
     }
 
     fun setupViews() {
+        bi.cometchatConversationList.setItemClickListener(object : OnItemClickListener<Conversation>() {
+            override fun OnItemClick(conversation: Conversation?, position: Int) {
+                var cometDoctor = conversation!!.conversationWith as User
 
+                var doctor = DoctorModel(
+                    doctorId = cometDoctor.uid,
+                    name = cometDoctor.name
+                )
+
+                var intent = Intent(activity, ChatActivity::class.java)
+                intent.putExtra("doctor", doctor)
+                startActivity(intent)
+            }
+        })
+    }
+
+    fun setLoggedInUI() {
+        if (CometChat.getLoggedInUser() != null) {
+            var conversationsRequest = ConversationsRequest.ConversationsRequestBuilder()
+                .setLimit(50)
+                .setConversationType(CometChatConstants.CONVERSATION_TYPE_USER)
+                .build()
+
+            conversationsRequest.fetchNext(object : CometChat.CallbackListener<List<Conversation>>() {
+                override fun onSuccess(conversations: List<Conversation>?) {
+                    bi.cometchatConversationList.setConversationList(conversations)
+                }
+
+                override fun onError(ex: CometChatException?) {
+                    Snackbar.make(bi.root, ex?.localizedMessage ?: "Couldn't load conversations list", Snackbar.LENGTH_SHORT).show()
+                }
+            })
+        }
     }
 }
